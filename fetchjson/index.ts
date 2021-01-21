d4bfd22c0bb840ca6db2654818083533acb8b374
brendino500 <brenda.ty@live.com>
2021-01-21 10:46:01 +0000

    added interface

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,6 +2,22 @@ import axios from 'axios'
 
 const  url = 'http://jsonplaceholder.typicode.com/todos/1'
 
+// defines the structure of an object
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  console.log(response.data)
+  const todo = response.data as Todo;
+  const id = todo.id
+  const title = todo.title
+  const finished = todo.completed
+
+  console.log(`
+    The Todo with ID:${id}
+    Has a title of: ${title}
+    Completed: ${finished}
+  `)
 })
